6b78c6e0244ff9b3eab26e1e984683670968926e
ThanTuan399 <thanminhtuan13579@gmail.com>
2026-09-14 22:20:26 +0700

    refactor: load organized frontend styles

--- FE/src/main.jsx
+++ FE/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import './styles/global.css';
+import './styles/layout.css';
+import './styles/home.css';
+import './styles/login.css';
+import './styles/booking.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
