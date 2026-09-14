bfbcf0320d1094389728ac8886e99631b8ed5d0b
ThanTuan399 <thanminhtuan13579@gmail.com>
2026-09-14 22:20:53 +0700

    refactor: ensure organized styles load after legacy styles

--- FE/src/main.jsx
+++ FE/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import App from './App.jsx';
 import './styles/global.css';
 import './styles/layout.css';
 import './styles/home.css';
 import './styles/login.css';
 import './styles/booking.css';
-import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
